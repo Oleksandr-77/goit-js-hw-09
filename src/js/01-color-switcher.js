c42e3081ecde6d9269d0d46d6faeb3e39046cc4c
Oleksandr Kokhno <kohnoan@gmail.com>
2023-03-19 01:33:23 +0200

    update01 19.03.23

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,22 @@
+function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215).toString(16).padStart(6, 0)}`;
+}
 
+let timerId = null;
+
+const refs = {
+    startBtn: document.querySelector('button[data-start]'),
+    stopBtn: document.querySelector('button[data-stop]'),
+}
+
+refs.startBtn.addEventListener('click', () => {
+    refs.startBtn.disabled = true;
+    timerId = setInterval(() => {
+    document.body.style.background = getRandomHexColor();
+    }, 1000);
+});
+
+refs.stopBtn.addEventListener("click", () => {
+    refs.startBtn.disabled = false;
+    clearInterval(timerId);
+});
